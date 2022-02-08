46aec50ab525492b84bfa769cc5325b30e5c786f
Antonio Sanchez <asanchezpu@saludcastillayleon.es>
2022-02-08 13:19:37 +0100

    Added flush to StreamToLogger

--- KoopaML.py
+++ KoopaML.py
@@ -58,6 +58,9 @@ class StreamToLogger(object):
 		for line in buf.rstrip().splitlines():
 			self.logger.log(self.log_level, line.rstrip())
 
+	def flush(self):
+		pass
+
 #Luigi Tasks
 class LoadDatabase(luigi.Task):
 	def run(self):
